317908a9fdbe2c4db8798be16b589ff0a79d5616
AnnLook <annlook23@gmail.com>
2022-08-17 19:46:49 -0400

    Created Streets

--- src/test/groovy/com/learning/basics/StreetSpec.groovy
+++ src/test/groovy/com/learning/basics/StreetSpec.groovy
@@ -10,4 +10,46 @@ public class StreetSpec extends Specification {
         then:
         assert street1.getArea() == 1800
     }
+    def "Create a streetCR" () {
+        when:
+       /*
+        List <Room>
+        List <Room> 540CoddingRoadRoom = [
+                new Room ("White", 7, 8, 4, 2, 1),
+                new Room ("Yellow", 9, 9, 4, 2, 2),
+                new Room ("Brown", 4, 3, 4, 1, 1),
+                new Room ("White", 6, 2, 4, 1, 1),
+                new Room ("White", 8, 8, 4, 2, 1),
+                new Room ("White", 7, 8, 4, 2, 1),
+                new Room ("White", 7, 8, 4, 3, 1),
+                new Room ("White", 7, 7, 4, 2, 1)
+        ]
+        List <Room> 544CoddingRoadRoom = [
+                new Room ("White", 7, 8, 4, 2, 1),
+                new Room ("Yellow", 9, 9, 4, 2, 2),
+                new Room ("Brown", 4, 3, 4, 1, 1),
+                new Room ("White", 6, 2, 4, 1, 1),
+                new Room ("Blue", 8, 8, 4, 2, 2),
+                new Room ("Pink", 4, 8, 4, 2, 1),
+                new Room ("White", 7, 8, 4, 4, 1),
+                new Room ("White", 7, 7, 4, 2, 1)
+        ]
+        */
+        List houses = [
+                new House ("540 Codding Road", 8, 3, null, "30 years tar", "Black", 18, 16, 20),
+                new House ("544 Codding Road", 9, 3, null, "30 years tar", "Black", 18, 20, 18)
+        ]
+        def street = new Street ("Codding Road", houses, 2, 900, "dead end street")
+
+        then: street.getNumberOfHouses () == 2
+    }
+    def "Create a Street2" () {
+        when:
+        List houses = [
+                new House ("320 Central Ave", 4, 2, null, "15 years tar", "Brown", 15, 14, 20),
+                new House ("324 Central Ave", 5, 2, null,"10 years tar", "Brown", 16, 14, 19)
+        ]
+        def street = new Street ("Central Ave", houses, 4, 12000, "dead end street")
+        then: street.getNumberOfHouses() == 2
+    }
 }
