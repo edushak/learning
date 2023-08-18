faaa1cd4a3439764a3911148bad126fe0a6d5150
edush <Kovbasa8>
2023-08-17 21:58:31 -0400

    switched to TestNG

--- src/test/java/com/learning/basics/CalculatorTest.java
+++ src/test/java/com/learning/basics/CalculatorTest.java
@@ -1,14 +1,18 @@
 package com.learning.basics;
 
-import org.junit.Assert;
-import org.junit.Test;
+//import org.junit.Assert;
+//import org.junit.Test;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
 public class CalculatorTest {
 
     Calculator calc = Calculator.getInstance();
 
     @Test
-    public void testAdd() throws Exception {
+    public void testAdd() {
         Assert.assertEquals(5, calc.add(2,3));
     }
 
