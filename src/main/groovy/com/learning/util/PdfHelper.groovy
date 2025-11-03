54ef365c4755e6e4d721b00813b7561f3f72f827
edushak <edward.dushak@gmail.com>
2025-11-02 20:36:51 -0500

    upgraded pdfbox API to v3.x

--- src/main/groovy/com/learning/util/PdfHelper.groovy
+++ src/main/groovy/com/learning/util/PdfHelper.groovy
@@ -1,31 +1,16 @@
 package com.learning.util
 
-import java.io.File;
-import java.io.IOException;
+import org.apache.pdfbox.Loader
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 class PdfHelper {
 
     static String read(File pdfFile) {
-        PDDocument document = PDDocument.load(pdfFile);
-        try {
-            if (!document.isEncrypted()) {
-                PDFTextStripper stripper = new PDFTextStripper();
-                String text = stripper.getText(document);
-                // System.out.println("Text:" + text);
-                return text
-            } else {
-                System.out.println("PDF document $pdfFile is encrypted. Cannot read!");
-            }
-        } catch(Exception ex) {
-            System.err.println("Something bad happened!");
-            System.err.println(ex.toString());
-        } finally {
-            // document?.close();
+        try (PDDocument document = Loader.loadPDF(pdfFile)) {
+            PDFTextStripper pdfStripper = new PDFTextStripper()
+            return pdfStripper.getText(document)
         }
-        return null
     }
 }
 
